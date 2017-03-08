50943adbb63a4b11d497745d6b4a19bcc415ef8b
hyjiacan <me@hyjiacan.com>
2017-03-08 09:31:19 +0800

    文档页面添加多说评论功能

--- examples/misc/js/layout.js
+++ examples/misc/js/layout.js
@@ -1,10 +1,11 @@
-function fixHeader() {var _hmt = _hmt || [];
-	(function() {
-	  var hm = document.createElement("script");
-	  hm.src = "https://hm.baidu.com/hm.js?3b889148d3a97c2e11263168434b1788";
-	  var s = document.getElementsByTagName("script")[0]; 
-	  s.parentNode.insertBefore(hm, s);
-	})();
+function fixHeader() {
+    var _hmt = _hmt || [];
+    (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?3b889148d3a97c2e11263168434b1788";
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(hm, s);
+    })();
     var resizeHandler;
     $(window).resize(function() {
         clearTimeout(resizeHandler);
@@ -17,4 +18,44 @@ function fixHeader() {var _hmt = _hmt || [];
             }
         }, 100);
     }).resize();
+
+    (function() {
+        function hash(str) {
+            str = str.toLowerCase();
+            // 1315423911=b'1001110011001111100011010100111'
+            var code = 1315423911,
+                i, ch;
+            for (i = str.length - 1; i >= 0; i--) {
+                ch = str.charCodeAt(i);
+                code ^= ((code << 5) + ch + (code >> 2));
+            }
+
+            return (code & 0x7FFFFFFF);
+        }
+
+        var container = $('#content');
+        if (!container.length) {
+            container = $(document.body);
+        }
+        container.append($('<div>')
+            .addClass("ds-thread")
+            .attr({
+                'data-thread-key': hash(window.location.href),
+                'data-title': document.title,
+                'data-url': window.location.href
+            }));
+
+        window.duoshuoQuery = {
+            short_name: "tinyselect"
+        };
+
+        $(document.head).append($('<script>').attr({
+            type: 'text/javascript',
+            async: true,
+            src: (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.unstable.js',
+            charset: 'UTF-8'
+        }));
+
+        $(document.body).append('<a class="duoshuobar" href="#ds-thread">留言</a>');
+    })();
 }
